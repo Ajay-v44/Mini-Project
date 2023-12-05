3b7eefe5a70ada45acb105edc03c4ea9c5e6ba58
Ajay v <vajay4834@gmail.com>
2023-12-05 18:11:54 +0530

    addresss form validation added

--- Mini-Cart-main/home/static/js/validation.js
+++ Mini-Cart-main/home/static/js/validation.js
@@ -14,7 +14,6 @@ const validatePassword = (password) => {
   );
 };
 
-
 //Register user
 
 function registerForm() {
@@ -62,16 +61,68 @@ function registerForm() {
 // Add Blogs
 
 function blogForm() {
-    var title = document.forms["blogform"]["title"].value;
-    var description=document.forms["blogform"]['description'].value;
-    if (!checkWhitespace(title)) {
-      alert("Title cannot be empty");
+  var title = document.forms["blogform"]["title"].value;
+  var description = document.forms["blogform"]["description"].value;
+  if (!checkWhitespace(title)) {
+    alert("Title cannot be empty");
+    return false;
+  }
+  if (!checkWhitespace(description)) {
+    alert("description  cannot be empty");
+    return false;
+  }
+
+  return true;
+}
+
+// address form
+function checkWhitespace(value) {
+  return value.trim() !== "";
+}
+
+function addressForm() {
+  var mob = document.forms["addressform"]["mob"].value;
+  var expr = /^[6-9][0-9]{9}$/;
+  if (isNaN(mob)) {
+    alert("Mobile number must be a digit");
+    return false;
+  } else if (!expr.test(mob) || mob.length != 10) {
+    alert("Invalid Phone Number");
+    return false;
+  } else {
+    if (!checkWhitespace(mob)) {
+      alert("Mobile cannot be empty");
       return false;
     }
-    if (!checkWhitespace(description)) {
-        alert("description  cannot be empty");
-        return false;
-      }
+  }
 
-    return true;
+  var altermob = document.forms["addressform"]["altermob"].value;
+  if (
+    !checkWhitespace(altermob) ||
+    isNaN(altermob) ||
+    altermob.length !== 10 ||
+    !expr.test(altermob)
+  ) {
+    alert("Invalid Alternate Number");
+    return false;
+  }
+  var pincodeRegx = "^[1-9]{1}[0-9]{2}\\s{0, 1}[0-9]{3}$";
+  var pincode = document.forms["addressform"]["pincode"].value;
+  if (
+    !checkWhitespace(pincode) ||
+    isNaN(pincode) ||
+    pincode.length !== 6 ||
+    !pincodeRegx.test(pincode)
+  ) {
+    alert("Invalid Pincode");
+    return false;
+  }
+
+  var address = document.forms["addressform"]["address"].value;
+  if (!checkWhitespace(address) || address.length < 10) {
+    alert("Invalid Address");
+    return false;
+  }
+
+  return true;
 }
